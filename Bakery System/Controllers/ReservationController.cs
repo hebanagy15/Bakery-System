ebf93f4146ac057659f3eb7e40b576df9d2b1308
MariamBaly <mariambaly912@gmail.com>
2025-04-29 00:41:53 +0300

    ADD RESERVATION CONTROLLER AND VIEW

---
+++ Bakery System/Controllers/ReservationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Bakery_System.Models;
+
+namespace Bakery_System.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Reservation/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Reservation/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                reservation.CreationDate = DateTime.Now;
+
+                reservation.TableId = 1;
+                reservation.CustomerId = 1;
+
+                _context.Reservations.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("ReservationSuccess");
+            }
+
+            return View(reservation);
+        }
+
+        public IActionResult ReservationSuccess()
+        {
+            return View();
+        }
+    }
+}
