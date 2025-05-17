2cc940f741987422c5e61bd56af8f77b2cd7d21a
hebanagy15 <hebanagy3333@gmail.com>
2025-05-17 12:10:52 +0200

    Sign Up and Sign In (Front End)

---
+++ Bakery System/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
